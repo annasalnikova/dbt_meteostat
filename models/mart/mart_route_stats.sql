571cac9da506967ad1630f70aaeb81e972a0db1a
Anna Salnikova <annsaln96@gmail.com>
2026-02-12 15:36:33 +0100

    mart_route_stats 2

--- models/mart/mart_route_stats.sql
+++ models/mart/mart_route_stats.sql
@@ -11,21 +11,21 @@ WITH route_data AS (
 		)
 SELECT origin
 		, dest
-		, a_origin.name
-		, a_origin.city
-		, a_origin.country
-		, a_dest.name
-		, a_dest.city
-		, a_dest.country
+		, a_origin.name AS origin_name
+		, a_origin.city AS origin_city
+		, a_origin.country AS origin_country
+		, a_dest.name AS dest_name
+		, a_dest.city AS dest_city
+		, a_dest.country AS dest_country
 		, COUNT(*) AS total_flights
-		, COUNT(DISTINCT tail_number)
-		, COUNT(DISTINCT airline)
-		, AVG(actual_elapsed_time)
-		, AVG(arr_delay)
-		, MAX(arr_delay)
-		, MAX(arr_delay)
-		, COUNT(cancelled)
-		, COUNT(diverted)
+		, COUNT(DISTINCT tail_number) AS n_tail_number
+		, COUNT(DISTINCT airline) AS n_airline
+		, AVG(actual_elapsed_time) AS avg_actual_elapsed_time
+		, AVG(arr_delay) AS avg_arr_delay
+		, MAX(arr_delay) AS max_arr_delay
+		, MIN(arr_delay) AS min_arr_delay
+		, COUNT(cancelled) AS amm_cancelled
+		, COUNT(diverted) AS amm_diverted
 FROM route_data f
 LEFT JOIN {{ref('prep_airports')}} a_origin
 ON f.origin = a_origin.faa
