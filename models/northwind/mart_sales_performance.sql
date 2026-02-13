ecd415bc2bc7c59f8c958c21fd6465694db89fb1
Anna Salnikova <annsaln96@gmail.com>
2026-02-13 17:10:35 +0100

    mart_sales_performance.sql

--- models/northwind/mart_sales_performance.sql
+++ models/northwind/mart_sales_performance.sql
@@ -7,9 +7,9 @@ SELECT date_part('year', order_date)
 		, SUM(revenue)
 		, COUNT(order_id)
 		, AVG(revenue)
-FROM prep p
+FROM prep
 LEFT JOIN {{ref('staging_categories')}} c
-ON p.category_id = c.category_id
+ON prep.category_id = c.category_id
 GROUP BY date_part('year', order_date)
 		, date_part('month', order_date)
 		, category_name
