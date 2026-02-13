20e25cb338b84ba41b136662eb23533c622f291e
Anna Salnikova <annsaln96@gmail.com>
2026-02-13 15:38:49 +0100

    prep_sales.sql

---
+++ models/northwind/prep_sales.sql
@@ -0,0 +1,22 @@
+WITH order_details AS (
+SELECT *
+FROM {{ source('northwind', 'order_details') }}
+),
+orders AS (
+SELECT *
+FROM {{ source('northwind', 'orders') }}
+),
+products AS (
+SELECT *
+FROM {{ source('northwind', 'products') }}
+)
+SELECT d.*
+		, o.*
+		, p.*
+		, d.unit_price * d.quantity * (1 - d.discount) AS revenue
+		, date_part('year', order_date)
+FROM order_details d
+LEFT JOIN orders o
+ON d.order_id = o.order_id
+LEFT JOIN products p
+ON d.product_id = p.product_id;
