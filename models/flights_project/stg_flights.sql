68568074d18fed2deea0c3ff8f6850c0bfbcaa0f
Anna Salnikova <annsaln96@gmail.com>
2026-02-16 11:50:56 +0100

    mart_sales_performance.sql

--- models/flights_project/stg_flights.sql
+++ models/flights_project/stg_flights.sql
@@ -1,5 +1,5 @@
 WITH flights_one_month AS (
         SELECT * 
-        FROM {{source('flights_project', 'air_force')}}
+        FROM {{source('air_force', 'flights_project')}}
     )
     SELECT * FROM flights_one_month
